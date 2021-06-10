8f2f34c1463b0536d4b225b9ea6e775b63da7a66
skykchmin <skykchmin@naver.com>
2021-06-10 23:34:08 +0900

    [210610] multer 이용한 이미지 업로드
    [210610] multer 이용한 이미지 업로드

--- lecture/app.js
+++ lecture/app.js
@@ -9,6 +9,8 @@ const dotenv = require('dotenv');
 dotenv.config(); // dotenv는 require 한다음, 최대한 위에 적어주는 것이 좋다.
 const pageRouter = require('./routes/page');
 const authRouter = require('./routes/auth');
+const postRouter = require('./routes/post');
+
 
 const app = express();
 app.set('port', process.env.PORT || 8001);
@@ -19,7 +21,10 @@ nunjucks.configure('views', {
 });
 
 app.use(morgan('dev'));
+
+// static: 요청과 실제 파일 주소가 다를 때 역할을 해주는 것
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('img', express.static(path.join(__dirname, 'uploads'))); // 사람들은 이미지 를 요청하지만 실제 폴더에는 images를 요청
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
@@ -36,6 +41,7 @@ app.use(session({
 // 라우터 연결
 app.use('/', pageRouter);
 app.use('/auth', authRouter);
+app.use('/post', postRouter);
 
 // 404 처리 미들웨어
 app.use((req, res, next) => {
---
+++ lecture/routes/post.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
+const { Post, Hashtag } = require('../models');
+const { isLoggedIn } = require('./middlewares');
+
+const router = express.Router();
+
+try {
+  fs.readdirSync('uploads');
+} catch (error) {
+  console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
+  fs.mkdirSync('uploads');
+}
+
+// multer 설정
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, cb) { // uploads 폴더에 이미지 업로드를 할 것이고
+      cb(null, 'uploads/');
+    },
+    filename(req, file, cb) {
+      const ext = path.extname(file.originalname);
+      cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    }, // 파일명은 파일명에다가 날짜를 넣어서 만든다. 덮어씌우기 방지
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
+
+router.post('/img', isLoggedIn, upload.single('img'), (req, res) => {
+  console.log(req.file);
+  res.json({ url: `/img/${req.file.filename}` }); 
+}); // ﻿form에서 'img' 라는 키로 이미지를 업로드를 해줘야합니다. 업로드 하고 난 후에 미들웨어를 실행하게 되는데 업로드한 파일 결과물이 req.file 안에 적혀있을 것이고 url을 다시 프론트로 돌려보내줍니다.
+
+const upload2 = multer();
+router.post('/', isLoggedIn, upload2.none(), async (req, res, next) => {
+  try {
+    console.log(req.user);
+    const post = await Post.create({
+      content: req.body.content,
+      img: req.body.url,
+      UserId: req.user.id,
+    });
+    const hashtags = req.body.content.match(/#[^\s#]*/g);
+    if (hashtags) {
+      const result = await Promise.all(
+        hashtags.map(tag => {
+          return Hashtag.findOrCreate({
+            where: { title: tag.slice(1).toLowerCase() },
+          })
+        }),
+      );
+      await post.addHashtags(result.map(r => r[0]));
+    }
+    res.redirect('/');
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+module.exports = router;
